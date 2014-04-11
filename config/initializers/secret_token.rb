97c4c3d523f9b21fa43f4d7258ea2d7f577c5840
Nizmox <nizmox@gmail.com>
2014-04-11 11:06:09 +1000

    initial demochat setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demochat::Application.config.secret_key_base = '2a2348b7646f44fb7cd6a28769bd0584e8a43cc7685fe53f3d440788288bc3f363050b09097b72400be43a3579c809fbeb7b6a9fe292ed83c99e60511748c053'
