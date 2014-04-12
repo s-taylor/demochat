b6f71755fc5c82ea434192adb84e9b89be8bc0b1
Nizmox <nizmox@gmail.com>
2014-04-12 12:40:07 +1000

    refactored and commented javascript

--- app/assets/javascripts/demochat.js
+++ app/assets/javascripts/demochat.js
@@ -1,26 +1,28 @@
 $(document).ready(function () {
-  var $msgform = $('#msgform');
-  demoChat.$messagesList = $('ul');
+  var $msgInput = $('#msg-input');
+
+  demoChat.$messagesList = $('ul#messages');
 
   // when the page loads, get the messages
   demoChat.getMessages();
 
-  $msgform.on('click','button',function (event) {
-    // test
-    console.log("click detected");
+  $('#msg-form').on('submit', function (event) {
     // prevent default form submit
     event.preventDefault();
     // grab text from the input box
-    var msgText = $msgform.find('input').val();
-    $msgform.find('input').val('');
+    var msgText = $msgInput.val();
+    //clear the input box
+    $msgInput.val('');
 
+    //send ajax request to post message content
     $.ajax({
       url: '/messages',
       method: 'POST',
       dataType: 'json',
       data: {message: {text: msgText}}
     }).done(function(response){
-      console.log("message created",response);
+      //add this message to the page
+      demoChat.addMessage(response);
     });
   });
 });
@@ -31,24 +33,36 @@ var demoChat = {
 
     //fetch the messages from the server
     getMessages: function(){
-      var self = this
+      //to refer to parent object
+      var self = this;
+
+      //submit ajax request to fetch all messages
       $.ajax({
         url: '/messages', 
         type: 'GET', 
         dataType: 'json'
       }).done(function(response){
-        // console.log(response);
+        // add the message to the page
         self.displayMessages(response);
       });
     },
 
     //update the page to display the messages
     displayMessages: function(messages) {
-      var self = this
+      //to refer to parent object
+      var self = this;
+      //loop through all messages
       _.each(messages, function (message) {
-        console.log(message);
-        var date = moment(message.created_at).calendar();
-        self.$messagesList.append(['<li>',date,': ',message.text,'</li>'].join(''));
+        // add the message to the page
+        self.addMessage(message);
       });
+    },
+
+    //takes a message object and adds it to the page
+    addMessage: function (message) {
+      //reformat the date
+      var date = moment(message.created_at).calendar();
+      //append all messages to the ul
+      this.$messagesList.append(['<li>',date,': ',message.text,'</li>'].join(''));
     }
 };
