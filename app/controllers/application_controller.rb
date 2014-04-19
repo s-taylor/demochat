fe11c4b94010d468fd483754ecea0d302126e7f8
Nizmox <nizmox@gmail.com>
2014-04-19 15:46:19 +1000

    implemented redirection to previous page after sign in and sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,32 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  after_filter :store_location
+
+  #-----------------------------------------
+  #Devise: Redirects user to previous URL after sign in and sign out
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    if (request.fullpath != "/users/sign_in" &&
+        request.fullpath != "/users/sign_up" &&
+        request.fullpath != "/users/password" &&
+        request.fullpath != "/users/sign_out" &&
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || home_path
+  end
+
+  def after_sign_out_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
 #-----------------------------------------
-  #added code to permit username with devise
+  #Devise: Adds additional permitted params to the devise user model
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
@@ -15,6 +39,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password,
       :password_confirmation, :current_password, :image, :image_cache) }
   end
-  #end of devise added code
   #-----------------------------------------
 end
